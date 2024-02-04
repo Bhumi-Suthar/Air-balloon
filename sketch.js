d64eb6182597253177244e7ec2b416123a129fe0
Bhumi-Suthar <71187354+Bhumi-Suthar@users.noreply.github.com>
2024-02-04 22:43:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
+
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloonImage1=loadAnimation("Images/HotAirBallon01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon01.png","Images/HotAirBallon01.png",
+   "Images/HotAirBallon01.png","Images/HotAirBallon02.png","Images/HotAirBallon02.png",
+   "Images/HotAirBallon02.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png");
+  }
+
+//Function to set initial environment
+function setup() {
+
+   database=firebase.database();
+
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,650,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  
+
+
+  textSize(20); 
+}
+
+
+function draw() {
+  background(bg);
+
+  
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+ 
